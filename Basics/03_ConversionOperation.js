254bac447a6304dff41e22a67f14ef986a33153e
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-19 13:39:16 +0000

    Conversion AND Operation

---
+++ Basics/03_ConversionOperation.js
@@ -0,0 +1,11 @@
+// Variable Declaration
+let score = "33";
+
+//Checking the type of the variable
+console.log(typeof score);
+// console.log(typeof(score));
+
+//Converting the String to Number
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
