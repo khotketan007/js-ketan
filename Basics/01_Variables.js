5d7d1d0de3897fb0d30bb10ce5fca98d3a2fd368
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-19 13:25:08 +0000

    Data Types and Variables

---
+++ Basics/01_Variables.js
@@ -0,0 +1,16 @@
+//Constant Varible Declaration
+const accountId = 1234;                     //Contant Variable(Immutable)
+// accountId = 66; no Executable
+
+// Two Ways to Declare the Varibale in JS
+let accountEmail = "Ketam003@gmail.com";    //1st type variable(Mutable)
+var accountPassward = "2345";               //2nd type to declare variable
+// accountCity = "Pune";
+let accountState
+
+/*
+prefere not to use Var
+Because of issues in block scope and functional scope
+*/ 
+
+console.table([accountId, accountEmail, accountPassward, accountState]);
---
+++ Basics/02_Datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //Treat all JS code as Newer Version
+
+// alert(2 + 3) --> we are using Node.js not Browser
+
+// Datatypes
+let name = "ketan"      //String
+let age  = 25           //Number -> (int, bigint)
+let isPresent = true    //Boolean -> (true, false)
+// null -> (standalone value)
+// undefined -> empty value indication 
+// symbol -> to find uniqueness 
+
+// object
+
+// console.log(typeof age);
