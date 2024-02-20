b544be3da7f469c1e662c392ebe02ba63b938f17
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-20 06:04:47 +0000

    Conversion and Operation

--- Basics/03_ConversionOperation.js
+++ Basics/03_ConversionOperation.js
@@ -2,13 +2,13 @@
 let score = "33";
 
 // Checking the type of the variable
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
 // Converting the String to Number
 let valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 // "33" => 33
 // "33se" => NaN
@@ -17,23 +17,23 @@ let valueInNumber = Number(score);
 // this is for boolean operation
 let istrue = 1
 let check = Boolean(istrue)
-// console.log(istrue);
+console.log(istrue);
 
 // **********Operations*************
-// console.log("Modulo : ",2 % 3)
-// console.log("Division : ",2 / 3);
+console.log("Modulo : ",2 % 3)
+console.log("Division : ",2 / 3);
 
-//String Operation
+// String Operation
 let str1 = "Hi"
 let str2 = " Ketan"
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
 // Complex Situation
-// console.log(1 + "2");
-// console.log("1" + 2);
-// console.log("1" + 2 + 2);
-// console.log(1 + 3 + "0");
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 3 + "0");
 
 // Tricky Converison
 console.log(+true);     // OP => 1
