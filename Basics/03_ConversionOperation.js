0c065ae7234ac814dc72489bb3e95c7e745126a8
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-20 06:00:49 +0000

    Conversion and Operation

--- Basics/03_ConversionOperation.js
+++ Basics/03_ConversionOperation.js
@@ -20,5 +20,25 @@ let check = Boolean(istrue)
 // console.log(istrue);
 
 // **********Operations*************
-console.log("Modulo : ",2 % 3)
-console.log("Division : ",2 / 3);
+// console.log("Modulo : ",2 % 3)
+// console.log("Division : ",2 / 3);
+
+//String Operation
+let str1 = "Hi"
+let str2 = " Ketan"
+let str3 = str1 + str2
+// console.log(str3);
+
+// Complex Situation
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 3 + "0");
+
+// Tricky Converison
+console.log(+true);     // OP => 1
+console.log(+"");       // OP => 0
+
+// Prefix and Postfix
+// prefix => ++varName
+// postfix => varName++
