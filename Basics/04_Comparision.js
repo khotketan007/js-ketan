8697d20533ee9c49265b8dac719e03ea7091f7b0
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-20 06:27:29 +0000

    Comparision Operator

---
+++ Basics/04_Comparision.js
@@ -0,0 +1,13 @@
+// = == === > < >= <= != 
+console.log("2" > 6); //true String > Number
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+
+//Strict Comparision 
+// === compare with value and with datatype
+console.log("=== Operator op => ","2" === 2);
