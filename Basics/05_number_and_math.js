8bbf09a4ebf708dda8432e095c3b354c75738d03
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-21 06:52:00 +0000

    Num and Math

--- Basics/05_number_and_math.js
+++ Basics/05_number_and_math.js
@@ -25,5 +25,18 @@ const newNumber = 10002340
 
 // *******************Maths*********************
 // console.log(Math);
-// Math Absolute function
-console.log(Math.abs(-5));
+// Math Absolute function(convert neg to pos)
+// console.log(Math.abs(-5));
+
+// Round function
+// console.log(Math.round(23.46));
+
+// Ceil function (Up Value Return)
+// console.log(Math.ceil(23.43));
+
+// floor function (Down Value Return)
+// console.log(`Floor Value : ${Math.floor(23.43)}`);
+
+// Min Max Function
+// console.log(`Min value in array : ${Math.min(4,2,5,6,77)}`);
+// console.log(`Max value in array : ${Math.max(4,2,5,6,7,34)}`);
