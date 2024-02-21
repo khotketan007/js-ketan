b5b6c1f9bafd0ecc1284e4207936004e692543a2
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-21 06:36:16 +0000

    Num and Math

--- Basics/05_number_and_math.js
+++ Basics/05_number_and_math.js
@@ -1,23 +1,29 @@
+//*********************Number**************************
 //This is the Number Datatype 
 //=> type is number
 const score = 500
-console.log(typeof score)
+// console.log(typeof score)
 
 //this is the Object datatype in number
 //=> type is object(number)
 const balance = new Number(500)
-console.log(typeof balance);
+// console.log(typeof balance);
 
 //Convert number to string
-console.log(balance.toString());
+// console.log(balance.toString());
 
 //Tofixed Property
-console.log(balance.toFixed(1));
+// console.log(balance.toFixed(1));
 
 //Precision property
 const otherNumber = 23.4843
-console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));
 
 //Tolocalestring (Adding commas in bewteen)
 const newNumber = 10002340
-console.log(newNumber.toLocaleString('en-IN'));
+// console.log(newNumber.toLocaleString('en-IN'));
+
+// *******************Maths*********************
+// console.log(Math);
+// Math Absolute function
+console.log(Math.abs(-5));
