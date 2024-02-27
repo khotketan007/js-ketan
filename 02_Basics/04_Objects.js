7b76bee49f6140ae1a824e850930648adb34ba50
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-27 05:46:18 +0000

    Object 4

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -3,6 +3,9 @@
 //Object Declaration
 const tinderUser = new Object()
 
+tinderUser.id = "124kssn"
+tinderUser.myName = "Ketan Tanaji Khot"
+tinderUser.myAge = 25
+tinderUser.myLocation = "Pune"
 
-
-console.log(tinderUser)
+console.log(tinderUser.myAge)
