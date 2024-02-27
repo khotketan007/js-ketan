d08ae1e30a51b8ec6dd7ca2f6c4ada688d4a0205
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-02-27 13:55:33 +0000

    Object 2

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -1,11 +1,53 @@
 // Constructor/Signleton object declaration
 
-//Object Declaration
+//Object Declaration (Singleton object)
 const tinderUser = new Object()
 
-tinderUser.id = "124kssn"
+tinderUser.id = "353Ketan5"
 tinderUser.myName = "Ketan Tanaji Khot"
 tinderUser.myAge = 25
 tinderUser.myLocation = "Pune"
+tinderUser.isLoggedIn = false
 
-console.log(tinderUser.myAge)
+// console.log(tinderUser);
+// console.log(tinderUser.myAge)
+
+// To Access the Keys in tinderUser following method
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// Entries to Delcare the Every value is in seperate object
+console.log(Object.entries(tinderUser));
+// To check the Value is present or not
+console.log(tinderUser.hasOwnProperty("id")); //=> It Return the Boolean value
+
+// New Object Declaration Non-SingleTon Object
+const regularUser = {
+    eMail : "KetanKhot25@gmail.com",
+    fullName : {
+        userFullName : {
+            firstName : "Ketan",
+            lastName : "Khot"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = {
+    1 : "A",
+    2 : "B"
+}
+const obj2 = {
+    3 : "C",
+    4 : "D"
+}
+
+// Combination of Two Arrays
+// Assign Operator to combine the Two object into single Objedt
+// This method also you use very rearly
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+//Another method to combine the two object in single object
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
