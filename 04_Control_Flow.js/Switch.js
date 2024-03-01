0ae9a5db16ca744633e54d359bf97701b637e6a1
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-01 15:07:23 +0000

    Switch Statement

---
+++ 04_Control_Flow.js/Switch.js
@@ -0,0 +1,33 @@
+// Switch Statement
+// Syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 2
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febrary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    
+    default:
+        console.log("Not in Calender");
+        break;
+}
