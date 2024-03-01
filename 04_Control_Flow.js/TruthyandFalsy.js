4b16f87d054572f03541f1a1baf6514ebade866f
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-01 15:16:08 +0000

    Truth and False Value

---
+++ 04_Control_Flow.js/TruthyandFalsy.js
@@ -0,0 +1,9 @@
+// const userEmail = []
+// if (userEmail) {
+//     console.log("Got User Email");
+// }else {
+//     console.log("Don't have User Email");
+// }
+
+// Falsy Value
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
