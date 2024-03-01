0e4370a3065501fb55fadf3436dbe42bed3755c5
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-01 11:02:27 +0000

    Control Flow

---
+++ 04_Control_Flow.js/One.js
@@ -0,0 +1,14 @@
+// If statement
+// Comparesion operator
+// > < <= >= != == ===(Check con and also check type also) !==
+// Simple if Statement 
+
+let score = 300
+if (score > 100) {
+    let power  = "Fly"
+    console.log(`User Power : ${power}`);
+}
+
+// Short Hand Notation of if Statement
+
+let balance = 1000
