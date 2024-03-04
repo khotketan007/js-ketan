60b9b2d36aeed329d9aab71ce11146ee9cd0e1f7
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-04 14:03:29 +0000

    For Ecah Loop

---
+++ 05_Iterations/forEachLoop.js
@@ -0,0 +1,44 @@
+// For Each Loop
+// Mostly for Ecah loop is Applied on the Array and Widly Used
+// Array Declaration
+let langauge = ["Java", "JabaScrit", "Cpp", "DotNet"]
+// For Ecah Loop
+// Simple function
+langauge.forEach( function (ArrayValues) {
+    // console.log(ArrayValues);
+} )
+
+// Now we Can use the Arrow Function
+langauge.forEach( (item) => {
+    // console.log(item);
+} )
+
+// Another Way to Print the Value
+// Function Declaration
+function printMe(CodingLangauge) {
+    // console.log(CodingLangauge);
+}
+langauge.forEach(printMe)
+
+// FroEach loop can have More methods eg => index arr
+langauge.forEach( (i, index, arr) => {
+    // console.log(i, " : ", index," : ", arr);
+} )
+
+// Now Looking for the Interestng thing 
+// Array Contain One or more Objects
+const myCoding = [
+    {
+        langaugeName : "Javascript",
+        langaugeFile : "js"
+    },
+    {
+        langaugeName : "Java",
+        langaugeFile : "java"
+    },
+    {
+        langaugeName : "Python",
+        langaugeFile : "py"
+    },
+
+]
