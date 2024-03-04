44984a070bb669e9bc7397688191474fba9fb5fd
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-04 08:06:38 +0000

    While do While js

---
+++ 05_Iterations/TwoWhileDWhile.js
@@ -0,0 +1,13 @@
+// While Loop
+let index = 0
+while (index <= 10) {
+    // console.log(index);
+    index = index + 2
+}
+
+let myArray = ["Batman", "Flash", "Superman"]
+let i = 0
+while (i < myArray.length) {
+    console.log(`Value in Array at index ${i} : ${myArray[i]}`);
+    i++
+}
