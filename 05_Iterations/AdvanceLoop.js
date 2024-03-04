8d053f19e33d5a7cf6777f41cf4ad71b790c24c8
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-04 13:20:49 +0000

    Advance Loop

--- 05_Iterations/AdvanceLoop.js
+++ 05_Iterations/AdvanceLoop.js
@@ -5,5 +5,30 @@
 const arr = [1, 2, 3, 4, 5]
 //for-of Loop
 for (let number of arr) {
-    console.log(number);
+    // console.log(number);
+}
+
+// Loop on Strings
+let greeting = "Ketan Khot"
+for (const greet of greeting) {
+    // If it contain space the break the Loop
+    if (greet == ' ') {
+        break
+    }
+    // console.log(greet);
+}
+
+// Maps in JavaScript
+// It is Known for the Key-value pair and it's unique
+// By creating the Object
+let newMap = new Map()
+newMap.set(1, "Ketan")
+newMap.set(2, "Snehal")
+newMap.set(3, "Nagesh")
+// print
+// console.log(newMap)
+
+// Now We are using for loop on Map
+for (const [key, value] of newMap) {
+    console.log(key, '=>', value);
 }
