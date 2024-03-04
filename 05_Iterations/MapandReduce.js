05e0a206931458f052bb722e1066cdf6c7a73bd2
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-04 15:13:00 +0000

    Filter Map and Reduce Method

---
+++ 05_Iterations/MapandReduce.js
@@ -0,0 +1,7 @@
+// Array Declaration
+const fruits = ["Apple", "Guava", "Graps", "Mango"]
+// Use for-Each loop
+let values = fruits.forEach( (items) => {
+    return items
+} )
+console.log(values); //=>Output : Undefined 
