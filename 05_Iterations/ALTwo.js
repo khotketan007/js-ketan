f4248499b6cacf1d1838e1f4f52d984e7c733139
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-04 13:34:47 +0000

    Advance Loop

---
+++ 05_Iterations/ALTwo.js
@@ -0,0 +1,16 @@
+// Apply loop on Object
+const myObject = {
+    js : 'JavaScript',
+    py : 'Python',
+    cpp : 'C++'
+}
+
+// To iterate the object you can apply the forIn Loop
+for (const key in myObject) {
+    // console.log(`${key} : ${myObject[key]}`);
+}
+
+const programming = ["js", "ruby", "java"]
+for (const key in programming) {
+    console.log(`Index ${}`);
+}
