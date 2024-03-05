53b13bf19f359ffbe6612c15e1ff450c770eb609
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-05 05:29:27 +0000

    Filter and Map and Reduce

--- 05_Iterations/MapandReduce.js
+++ 05_Iterations/MapandReduce.js
@@ -5,3 +5,20 @@ let values = fruits.forEach( (items) => {
     return items
 } )
 console.log(values); //=>Output : Undefined 
+
+// Number Array
+const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// Filter function
+// const newNums = myNumber.filter( (num) => {
+//    return num > 5
+// })
+
+// Using For each Loop
+// const newNums = []
+// myNumber.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
