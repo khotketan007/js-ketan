03708381ec48762328d2c5220e50e9687dbf13c5
khotketan007 <160468409+khotketan007@users.noreply.github.com>
2024-03-19 11:00:52 +0000

    Promise

--- 09_Advaance_one/promises.js
+++ 09_Advaance_one/promises.js
@@ -8,11 +8,67 @@ const promiseOne = new Promise(function(resolve, reject){
     // DB Calls, Cyrptography
     // Networkcall
     setTimeout(() => {
-        console.log('Async task is Complete');
+        // console.log('Async task is Complete');
         resolve()
     }, 2000)
 }) 
-
 promiseOne.then(function(){
-    console.log("Promise Consumed");
+    // console.log("Promise Consumed");
 })
+
+const promiseTwo = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        // console.log('Async task 2');
+        resolve()
+    },3000)
+})
+promiseTwo.then(() => {
+    // console.log("Promise Consumed 2");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        resolve({userName : "Chai", email : "Chai@Example.com"})
+    }, 1000)
+})
+promiseThree.then(function(data){
+    // console.log(data.email);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = true
+        if(!error){
+            resolve({userName : "Ketan@github", password : "Ketan@123"})
+        } else {
+            reject('Error! Data not Fetched')
+        }
+    }, 1000)
+})
+promiseFour.then((data) => {
+    // console.log(data);
+    return data.userName
+}).then((username) => {
+    // console.log(username);
+}).catch((error) => {
+    // console.log(error);
+}).finally(() => {
+    // console.log("The Promise Either Resolved or Rejected");
+})
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = true
+        if(!error){
+            resolve({userName : "Javascript", password : "js@123"})
+        } else {
+            reject('Error! JS Failed')
+        }
+    }, 1000)
+})
+async function consumePromiseFive(){
+    const response = await promiseFive
+    console.log(response);
+}
+
+consumePromiseFive()
